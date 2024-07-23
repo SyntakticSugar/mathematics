9108f238b35e2914366f683d8e3fe5eaac7c17de
SyntakticSugar <rhsculling@protonmail.com>
2024-07-23 17:39:03 +1200

    Some reorganising to put split up the construction and the arithmetic of the integers.

---
+++ Mathematics/Number/Integer/arithmetic.lean
@@ -0,0 +1,14 @@
+/-
+Module      : Arithmetic.
+Description : Fundamental arithmetic on ℤ
+Maintainer  : Robert Culling <rhsculling@pm.com>
+
+  ℤ, along with negation, +, * were constructed in:
+
+    Mathematics.Number.Integer.construction
+
+  This module proves basic theorems in the arithmetic
+  of the integers. Once these a proved, one should not
+  need to refer to the implementation details of ℤ
+  as a quotient of ℕ × ℕ.
+-/
