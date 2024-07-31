deffc14ad27d181b3bbceb153fc2a3a620ebabde
Robert Culling <rhsculling@protonmail.com>
2024-07-31 23:55:44 +1200

    Wrote (Comm)(Add)Monoids to the library as extensions of the semigroup type classes.

--- Mathematics/Algebra/monoid.lean
+++ Mathematics/Algebra/monoid.lean
@@ -1,3 +1,31 @@
+/-
+Module      : Monoids
+Description : Base of the Algebraic Heirachy
+Maintainer  : Robert Culling <rhsculling@pm.com>
+
+  (Comm)(Add)Monoids are defined to be extensions
+  of the corresponding (Comm)(Add)Semigroup classes.
+
+-/
+
 import Mathematics.Algebra.semigroup
 
 open Semigroup
+
+class Monoid (α : Type u) extends Semigroup α  where
+  one : α
+  one_mul : ∀ a : α,  one * a = a
+  mul_one : ∀ a : α,  a * one = a
+
+class CommMonoid (α : Type u) extends CommSemigroup α  where
+  one : α
+  one_mul : ∀ a : α,  one * a = a
+
+class AddMonoid (α : Type u) extends AddSemigroup α  where
+  zero : α
+  zero_add : ∀ a : α,  zero + a = a
+  add_zero : ∀ a : α,  a + zero = a
+
+class CommAddMonoid (α : Type u) extends CommAddSemigroup α  where
+  zero : α
+  zero_add : ∀ a : α,  zero + a = a
